10ff69636ffce6876bb3e6d07bf3439fdbf4db8e
f1sherkk <krzyk.kamil@gmail.com>
2024-09-05 01:37:53 +0200

    adjusted port and host for render hosting service

--- app.py
+++ app.py
@@ -143,4 +143,4 @@ def check_answer():
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
